cddc945f541581fc1c8db235c7b59e08e9eaebac
himanshupal21674 <himanshupal21674@gmail.com>
2023-11-02 11:31:34 +0530

    git-1 assignment location computer-1

---
+++ Introduction to Js/Pro2.js
@@ -0,0 +1,12 @@
+// Define a variable called name with your Name as the assigned value
+let name = "Himanshu Pal";
+console.log(name);
+
+let fathername = "Mr. Ram Prakash Pal";
+console.log(fathername);
+
+let mothername = "Mrs. Geeta Pal";
+console.log(mothername);
+
+let family = "My small familly";
+console.log(family);
---
+++ Introduction to Js/Pro4.js
@@ -0,0 +1,13 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+let studentinfo = [
+    name = "Himanshu Pal",
+    school = "Florets International School",
+    grade = "12th",
+    section = "A",
+    rollno = "1226",
+    subject = "english, computer, phy, bio, chem",
+    marksscored = "90, 88, 40, 70, 78",
+     ] ;
+   console.log(studentinfo);
